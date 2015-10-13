facf0c24107ea41d8130f1fa24ed4ac2d20d20b3
zedd45 <christopher.keen@gmail.com>
2015-10-13 18:19:14 -0400

    week 2: files v1

--- 02-hapi/index.js
+++ 02-hapi/index.js
@@ -1,4 +1,5 @@
 var Hapi = require('hapi');
+var Inert = require('inert');
 
 const PORT = 8080;
 
@@ -26,4 +27,23 @@ server.route({
 
         reply('you visited: ' + request.params.uriSegment || request.path);
     }
+});
+
+
+server.register(Inert, function (err) {
+
+   if (err) {
+        throw err;
+    }
+
+
+    server.route({
+        method: 'GET',
+        path: '/favicon.ico',
+        handler: function (request, reply) {
+
+            reply.file( __dirname + '/public/favicon.ico');
+        }
+    });
+
 });
